6066dd740e7c03fb38b4ade751f3f8d0b7eb330d
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-20 16:46:39 +0300

    Refactor PostRepo interface to use Either for error handling and improve method signatures

--- lib/features/posts/domain/repositories/post_repo.dart
+++ lib/features/posts/domain/repositories/post_repo.dart
@@ -1,8 +1,11 @@
 import 'package:clean_architecutre_posts_app/features/posts/domain/entities/post_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class PostRepo {
-  Future<List<PostEntity>> getPosts();
-  Future<void> createPost();
-  Future<void> updatePost();
-  Future<void> deletePost();
+  Future<Either<Failure, List<PostEntity>>> getAllPosts();
+  Future<Either<Failure, Unit>> createPost({required PostEntity post});
+  Future<Either<Failure, Unit>> updatePost({required PostEntity post});
+  Future<Either<Failure, Unit>> deletePost({required int postId});
 }
+
+class Failure {}
