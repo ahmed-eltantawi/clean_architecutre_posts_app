f9ca650133396165e91e7b14e2ee0f342e2991f2
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-20 16:03:34 +0300

    Refactor app theme and update main application structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:clean_architecutre_posts_app/core/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(debugShowCheckedModeBanner: false, theme: appTheme);
   }
 }
