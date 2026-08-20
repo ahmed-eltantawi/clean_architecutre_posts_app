26d8738dd70ddcefbeaff6c15dfc2272a2791f97
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-20 16:13:10 +0300

    Refactor main app structure and remove unused dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, theme: appTheme);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: Scaffold(),
+    );
   }
 }
