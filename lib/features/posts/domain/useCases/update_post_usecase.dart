c25a128be4b1c81732eb639629dba9bb62ca7843
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-20 18:59:55 +0300

    Add UpdatePostUsecase for updating posts in the application

---
+++ lib/features/posts/domain/useCases/update_post_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:clean_architecutre_posts_app/features/posts/domain/entities/post_entity.dart';
+import 'package:clean_architecutre_posts_app/features/posts/domain/repositories/post_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class UpdatePostUsecase {
+  final PostRepo postRepo;
+  UpdatePostUsecase(this.postRepo);
+
+  Future<Either<Failure, Unit>> call({required PostEntity post}) async {
+    return await postRepo.updatePost(post: post);
+  }
+}
