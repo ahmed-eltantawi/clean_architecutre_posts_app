7d29f3dab626327ff81146550f8c52eb417a22a9
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-22 23:38:14 +0300

    Implement caching for posts using SharedPreferences and update pubspec.yaml for shared_preferences dependency

--- lib/features/posts/data/datasources/post_local_data_source.dart
+++ lib/features/posts/data/datasources/post_local_data_source.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:clean_architecutre_posts_app/features/posts/data/models/post_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class PostLocalDataSource {
   Future<List<PostModel>> getAllCachedPosts();
@@ -7,15 +10,26 @@ abstract class PostLocalDataSource {
 }
 
 class PostLocalDataSourceImpl implements PostLocalDataSource {
+  // we take an instance of SharedPreferences to apply the dependency injection pattern
+  SharedPreferences sharedPreferences;
+
+  PostLocalDataSourceImpl({required this.sharedPreferences}); //Constructor
+
   @override
+  ///This Method is used to cache the posts in the local storage
   Future<Unit> cachePosts({required List<PostModel> posts}) {
-    // TODO: implement cachePosts
-    throw UnimplementedError();
+    // convert the List<PostModel> to List<Map<String, dynamic>>
+    List postModelsToJson = posts
+        .map<Map<String, dynamic>>((post) => post.toJson())
+        .toList();
+
+    // save the posts in the sharedPreferences(local storage)
+    sharedPreferences.setString("CACHED_POSTS", json.encode(postModelsToJson));
+
+    // return the unit if the operation is successful
+    return Future.value(unit);
   }
 
   @override
-  Future<List<PostModel>> getAllCachedPosts() {
-    // TODO: implement getAllCachedPosts
-    throw UnimplementedError();
-  }
+  Future<List<PostModel>> getAllCachedPosts() {}
 }
