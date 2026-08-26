5e1f7830129ec0d2ff358bb1895be7a0cf636a73
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-08-26 23:33:24 +0300

    Refactor main.dart: simplify BlocProvider setup by using service locator for GetPostsBloc and AddDeleteUpdatePostBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
+import 'package:clean_architecutre_posts_app/config/services/services_locator.dart';
 import 'package:clean_architecutre_posts_app/core/app_theme.dart';
-import 'package:clean_architecutre_posts_app/core/cache/cache_helper.dart';
-import 'package:clean_architecutre_posts_app/core/network/dio_consumer.dart';
-import 'package:clean_architecutre_posts_app/core/network/network_info.dart';
-import 'package:clean_architecutre_posts_app/features/posts/data/datasources/post_local_data_source.dart';
-import 'package:clean_architecutre_posts_app/features/posts/data/datasources/post_remote_data_source.dart';
-import 'package:clean_architecutre_posts_app/features/posts/data/repositories/post_repo_impl.dart';
-import 'package:clean_architecutre_posts_app/features/posts/domain/useCases/get_all_posts_usecase.dart';
+import 'package:clean_architecutre_posts_app/features/posts/presentation/bloc/add_delete_update_post_bloc/add_delete_update_post_bloc.dart';
 import 'package:clean_architecutre_posts_app/features/posts/presentation/bloc/get_posts_bloc/get_posts_bloc.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 late SharedPreferences sharedPreferences;
 void main() async {
-  sharedPreferences = await SharedPreferences.getInstance();
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupServiceLocator();
   runApp(const MyApp());
 }
 
@@ -25,22 +20,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (context) => GetPostsBloc(
-            getAllPostsUsecase: GetAllPostsUsecase(
-              postRepo: PostRepoImpl(
-                postRemoteDataSource: PostRemoteDataSourceImplWithDio(
-                  dioConsumer: DioConsumer(dio: Dio()),
-                ),
-                postLocalDataSource: PostLocalDataSourceImpl(
-                  sharedPreferences: sharedPreferences,
-                  cacheHelper: CacheHelper(),
-                ),
-                networkInfo: NetworkInfoImpl(),
-              ),
-            ),
-          ),
-        ),
+        BlocProvider(create: (context) => getIt<GetPostsBloc>()),
+        BlocProvider(create: (context) => getIt<AddDeleteUpdatePostBloc>()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
